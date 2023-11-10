0e25a5cd0d5b14be9c4cb3533aed7577e543e83b
Mohamad Elsayed <mhmd12elsayed@gmail.com>
2023-11-10 23:36:59 +0200

    final develop

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,13 @@
+//explored more about some methods using https://www.w3schools.com/
+
 //adding events to the main buttons
 const AddTaskbtn = document.getElementById("add-task");
 const Backbtn_1 = document.getElementById("back1");
 const Backbtn_2 = document.getElementById("back2");
 const Addbtn = document.getElementById("add");
 const ListTasks = document.getElementById("list-task");
+const priorityHigh= document.getElementById("high");
+const priorityNormal=document.getElementById("normal");
 let AddTaskWin = document.getElementById("add-task-window");
 let FeatureWin = document.getElementById("feature");
 let Tasks = document.getElementById("list");
@@ -14,6 +18,29 @@ Backbtn_1.addEventListener("click", Back);
 Backbtn_2.addEventListener("click", Back);
 Addbtn.addEventListener("click", AddT);
 ListTasks.addEventListener("click", List_task);
+//Priority toggle
+priorityHigh.addEventListener("change",function(){
+  if( priorityNormal.checked==true)
+  {
+  priorityNormal.checked=false;
+  }
+  else{
+    priorityNormal.checked=true;
+  }
+})
+priorityNormal.addEventListener("change",function(){
+  if(  priorityHigh.checked==true)
+  {
+  priorityHigh.checked=false;
+  }
+  else{
+    priorityHigh.checked=true;
+  }
+})
+
+
+
+
 
 function List_task() {
   FeatureWin.style.display = "none";
@@ -29,6 +56,7 @@ function AddTask() {
   left_side.style.display = "flex";
 }
 
+
 function Back() {
   AddTaskWin.style.display = "none";
   FeatureWin.style.display = "flex";
@@ -40,7 +68,7 @@ let end_date = document.getElementsByName("end-date")[0];
 let counter = 0;
 //adding/editing/deleting a task
 function AddT() {
-  if(f_name.value.trim()=="" || st_date.value=="" || end_date.value=="") {
+  if(f_name.value.trim()=="" ) {
     alert("Fill missing data");
   }
   else{
@@ -63,6 +91,7 @@ function AddT() {
     let save = document.createElement("button");
     save.innerText = "save";
     save.id = `save${counter}`;
+    save.classList.add("save");
     new_task.appendChild(save);
     save.addEventListener("click", function (e) {
       console.log(e.target.id);
@@ -70,8 +99,6 @@ function AddT() {
       new_task.childNodes[0].innerText = `${edit_input.value}`;
       new_task.removeChild(edit_input);
       new_task.removeChild(save);
-      //edited_name.value="";
-      //edited_name=[];
     });
   });
   del.classList.add("small-button-del");
@@ -91,24 +118,50 @@ function AddT() {
   active.type = "checkbox";
   active.id = `active${counter}`;
   active.name = "active-task";
-  active.value = "active";
+  active.checked=true;
   active.classList.add("checkbox");
   new_task.innerHTML = `<p class="task-name">${f_name.value}</p><p class="st-date">${st_date.value}</p><p class="end-date">${end_date.value}</p>`;
   edit.innerHTML = `<img src="./assets/icons/edit-solid.svg" alt="ed">`;
   del.innerHTML = `<img src="./assets/icons/trash-can-solid.svg" alt="tr">`;
   new_task.appendChild(completed);
   new_task.appendChild(active);
+  //check priority
+  if(document.getElementById("high").checked==true){
+    let prior=document.createElement("p");
+    prior.innerText="high";
+    prior.classList.add("prior");
+    new_task.appendChild(prior);
+  } else{
+    let prior=document.createElement("p");
+    prior.innerText="normal";
+    prior.classList.add("prior");
+    new_task.appendChild(prior);
+  }
   new_task.appendChild(edit);
   new_task.appendChild(del);
   Tasks.appendChild(new_task);
   f_name.value = "";
   st_date.value = "";
   end_date.value = "";
-  console.log(new_task.childNodes);
+  //task status toggle
+  let complete_task=document.getElementById(`completed${counter}`);
+  let active_task=document.getElementById(`active${counter}`);
+  complete_task.addEventListener("change",function(){
+    if(active_task.checked==true){
+    active_task.checked=false;
+    }else{
+      active_task.checked=true;
+    }
+  })
+  active_task.addEventListener("change",function(){
+    if(complete_task.checked==true){
+    complete_task.checked=false;
+    } else{
+      complete_task.checked=true;
+    }
+  })
    }
 }
 
-// if (document.getElementById(`completed${counter}`.checked)){
-//   document.getElementById(`active${counter}`.checked=false);
-// }
+
 
